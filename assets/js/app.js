11d955baf92116bfc797151a2da4e53febbebf7f
Andrzej Trzaska <atrzaska2@gmail.com>
2020-08-12 21:16:32 +0200

    Remove default assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,7 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
 import "../css/app.scss"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
 import "phoenix_html"
+
+$(function () {
+  $("a[rel~=popover], .has-popover").popover()
+  $("a[rel~=tooltip], .has-tooltip").tooltip()
+})
