5d7e663d4b0d80ba9c496917449ad8b5a3f84ca3
Andrzej Trzaska <atrzaska2@gmail.com>
2020-08-24 20:57:38 +0200

    Scope projects to user

--- lib/bugzilla_web/controllers/project_controller.ex
+++ lib/bugzilla_web/controllers/project_controller.ex
@@ -5,7 +5,7 @@ defmodule BugzillaWeb.ProjectController do
   alias Bugzilla.Projects.Project
 
   def index(conn, _params) do
-    projects = Projects.list_projects()
+    projects = Projects.list_projects(user: conn.assigns.current_user)
     render(conn, "index.html", projects: projects)
   end
 
@@ -15,7 +15,7 @@ defmodule BugzillaWeb.ProjectController do
   end
 
   def create(conn, %{"project" => project_params}) do
-    case Projects.create_project(project_params) do
+    case Projects.create_project(project_params, user: conn.assigns.current_user) do
       {:ok, project} ->
         conn
         |> put_flash(:info, "Project created successfully.")
@@ -27,18 +27,18 @@ defmodule BugzillaWeb.ProjectController do
   end
 
   def show(conn, %{"id" => id}) do
-    project = Projects.get_project!(id)
+    project = Projects.get_project!(id, user: conn.assigns.current_user)
     render(conn, "show.html", project: project)
   end
 
   def edit(conn, %{"id" => id}) do
-    project = Projects.get_project!(id)
+    project = Projects.get_project!(id, user: conn.assigns.current_user)
     changeset = Projects.change_project(project)
     render(conn, "edit.html", project: project, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "project" => project_params}) do
-    project = Projects.get_project!(id)
+    project = Projects.get_project!(id, user: conn.assigns.current_user)
 
     case Projects.update_project(project, project_params) do
       {:ok, project} ->
@@ -52,7 +52,7 @@ defmodule BugzillaWeb.ProjectController do
   end
 
   def delete(conn, %{"id" => id}) do
-    project = Projects.get_project!(id)
+    project = Projects.get_project!(id, user: conn.assigns.current_user)
     {:ok, _project} = Projects.delete_project(project)
 
     conn
