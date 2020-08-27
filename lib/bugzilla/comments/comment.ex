e329e5f47b19bf51996d48b21c1b8b23c61c2005
Andrzej Trzaska <atrzaska2@gmail.com>
2020-08-27 08:06:10 +0200

    Add counter cachce to comments

--- lib/bugzilla/comments/comment.ex
+++ lib/bugzilla/comments/comment.ex
@@ -1,6 +1,7 @@
 defmodule Bugzilla.Comments.Comment do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   alias Bugzilla.Accounts.User
   alias Bugzilla.Stories.Story
@@ -17,7 +18,15 @@ defmodule Bugzilla.Comments.Comment do
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:content])
-    |> validate_required([:content])
+    |> cast(attrs, [:content, :story_id])
+    |> validate_required([:content, :story_id])
+    |> prepare_changes(fn (changeset) ->
+      if story_id = get_change(changeset, :story_id) do
+        query = from Story, where: [id: ^story_id]
+        changeset.repo.update_all(query, inc: [comments_count: 1])
+      end
+
+      changeset
+    end)
   end
 end
