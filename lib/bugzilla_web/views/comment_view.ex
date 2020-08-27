249917da39af8d19e8facb770b9f2372b17df403
Andrzej Trzaska <atrzaska2@gmail.com>
2020-08-27 08:11:14 +0200

    Add select input helper to comments view

--- lib/bugzilla_web/views/comment_view.ex
+++ lib/bugzilla_web/views/comment_view.ex
@@ -1,3 +1,8 @@
 defmodule BugzillaWeb.CommentView do
+  import BugzillaWeb.Helpers.Form
   use BugzillaWeb, :view
+
+  def stories(conn) do
+    Bugzilla.Stories.list_for_select(user: conn.assigns.current_user)
+  end
 end
