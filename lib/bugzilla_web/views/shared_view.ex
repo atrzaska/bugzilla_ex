a4e561b1bdf9e43cf825d23daa9deaa4a0c4f8f6
Andrzej Trzaska <atrzaska2@gmail.com>
2020-08-28 22:18:34 +0200

    Add bootstrap nav item helper

--- lib/bugzilla_web/views/shared_view.ex
+++ lib/bugzilla_web/views/shared_view.ex
@@ -1,3 +1,7 @@
 defmodule BugzillaWeb.SharedView do
   use BugzillaWeb, :view
+
+  def bootstrap_nav_item(conn, title, to: to) do
+    PhoenixActiveLink.active_link(conn, title, to: to, wrap_tag: 'li', class: "nav-link", wrap_tag_opts: [class: "nav-item"])
+  end
 end
