8eafddb013c64744b16f16fdc2f7474c0d4dbca1
Andrzej Trzaska <atrzaska2@gmail.com>
2020-08-29 15:10:27 +0200

    Add owner helper to project view

--- lib/bugzilla_web/views/project_view.ex
+++ lib/bugzilla_web/views/project_view.ex
@@ -1,4 +1,10 @@
 defmodule BugzillaWeb.ProjectView do
   import BugzillaWeb.Helpers.Form
   use BugzillaWeb, :view
+
+  alias Bugzilla.UserProjects
+
+  def owner?(conn, project) do
+    UserProjects.owner?(user: conn.assigns.current_user, project: project)
+  end
 end
