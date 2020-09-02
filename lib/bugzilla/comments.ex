fec8e03a37b3da87260bdda27472117f06edf897
Andrzej Trzaska <atrzaska2@gmail.com>
2020-09-02 20:50:03 +0200

    New helper functions in comments context

--- lib/bugzilla/comments.ex
+++ lib/bugzilla/comments.ex
@@ -8,14 +8,18 @@ defmodule Bugzilla.Comments do
     Repo.all(Comment)
   end
 
+  def list_comments(story: story) do
+    from(c in Comment, where: c.story_id == ^story.id) |> Repo.all()
+  end
+
   def get_comment!(id, user: user) do
     Comment |> Repo.get_by!(user_id: user.id, id: id)
   end
 
   def get_comment!(id), do: Repo.get!(Comment, id)
 
-  def create_comment(attrs \\ %{}, user: user) do
-    %Comment{user_id: user.id}
+  def create_comment(attrs \\ %{}, user: user, story: story) do
+    %Comment{user_id: user.id, story_id: story.id}
     |> Comment.changeset(attrs)
     |> Repo.insert()
   end
