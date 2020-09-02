15449097aea9a537fea34f323d6be1c4ec4c9b0b
Andrzej Trzaska <atrzaska2@gmail.com>
2020-09-02 21:11:56 +0200

    Implement logic for comment and task controllers

--- lib/bugzilla_web/controllers/comment_controller.ex
+++ lib/bugzilla_web/controllers/comment_controller.ex
@@ -3,54 +3,64 @@ defmodule BugzillaWeb.CommentController do
 
   alias Bugzilla.Comments
   alias Bugzilla.Comments.Comment
-
-  def index(conn, _params) do
-    comments = Comments.list_comments()
-    render(conn, "index.html", comments: comments)
-  end
+  alias Bugzilla.Stories
 
   def new(conn, _params) do
     user = conn.assigns.current_user
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
     changeset = Comments.change_comment(%Comment{user_id: user.id})
-    render(conn, "new.html", changeset: changeset)
+
+    conn
+    |> assign(:story, story)
+    |> assign(:project, project)
+    |> render("new.html", changeset: changeset)
   end
 
   def create(conn, %{"comment" => comment_params}) do
     user = conn.assigns.current_user
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
 
-    case Comments.create_comment(comment_params, user: user) do
+    case Comments.create_comment(comment_params, user: user, story: story) do
       {:ok, _comment} ->
         conn
         |> put_flash(:info, "Comment created successfully.")
-        |> redirect(to: Routes.comment_path(conn, :index))
+        |> redirect(to: Routes.project_story_path(conn, :edit, project, story))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = conn.assigns.current_user
-    comment = Comments.get_comment!(id, user: user)
-    render(conn, "show.html", comment: comment)
-  end
-
   def edit(conn, %{"id" => id}) do
     user = conn.assigns.current_user
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
     comment = Comments.get_comment!(id, user: user)
     changeset = Comments.change_comment(comment)
-    render(conn, "edit.html", comment: comment, changeset: changeset)
+
+    conn
+    |> assign(:story, story)
+    |> assign(:project, project)
+    |> render("edit.html", comment: comment, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "comment" => comment_params}) do
     user = conn.assigns.current_user
     comment = Comments.get_comment!(id, user: user)
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
 
     case Comments.update_comment(comment, comment_params) do
       {:ok, _comment} ->
         conn
         |> put_flash(:info, "Comment updated successfully.")
-        |> redirect(to: Routes.comment_path(conn, :index))
+        |> redirect(to: Routes.project_story_path(conn, :edit, project, story))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", comment: comment, changeset: changeset)
@@ -60,10 +70,14 @@ defmodule BugzillaWeb.CommentController do
   def delete(conn, %{"id" => id}) do
     user = conn.assigns.current_user
     comment = Comments.get_comment!(id, user: user)
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
+
     {:ok, _comment} = Comments.delete_comment(comment)
 
     conn
     |> put_flash(:info, "Comment deleted successfully.")
-    |> redirect(to: Routes.comment_path(conn, :index))
+    |> redirect(to: Routes.project_story_path(conn, :edit, project, story))
   end
 end
--- lib/bugzilla_web/controllers/task_controller.ex
+++ lib/bugzilla_web/controllers/task_controller.ex
@@ -3,48 +3,64 @@ defmodule BugzillaWeb.TaskController do
 
   alias Bugzilla.Tasks
   alias Bugzilla.Tasks.Task
-
-  def index(conn, _params) do
-    tasks = Tasks.list_tasks()
-    render(conn, "index.html", tasks: tasks)
-  end
+  alias Bugzilla.Stories
 
   def new(conn, _params) do
+    user = conn.assigns.current_user
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
     changeset = Tasks.change_task(%Task{})
-    render(conn, "new.html", changeset: changeset)
+
+    conn
+    |> assign(:story, story)
+    |> assign(:project, project)
+    |> render("new.html", changeset: changeset)
   end
 
   def create(conn, %{"task" => task_params}) do
-    case Tasks.create_task(task_params) do
+    user = conn.assigns.current_user
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
+
+    case Tasks.create_task(task_params, user: user, story: story) do
       {:ok, _task} ->
         conn
         |> put_flash(:info, "Task created successfully.")
-        |> redirect(to: Routes.task_path(conn, :index))
+        |> redirect(to: Routes.project_story_path(conn, :edit, project, story))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    task = Tasks.get_task!(id)
-    render(conn, "show.html", task: task)
-  end
-
   def edit(conn, %{"id" => id}) do
-    task = Tasks.get_task!(id)
+    user = conn.assigns.current_user
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
+    task = Tasks.get_task!(id, user: user)
     changeset = Tasks.change_task(task)
-    render(conn, "edit.html", task: task, changeset: changeset)
+
+    conn
+    |> assign(:story, story)
+    |> assign(:project, project)
+    |> render("edit.html", task: task, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "task" => task_params}) do
-    task = Tasks.get_task!(id)
+    user = conn.assigns.current_user
+    task = Tasks.get_task!(id, user: user)
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
 
     case Tasks.update_task(task, task_params) do
       {:ok, _task} ->
         conn
         |> put_flash(:info, "Task updated successfully.")
-        |> redirect(to: Routes.task_path(conn, :index))
+        |> redirect(to: Routes.project_story_path(conn, :edit, project, story))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", task: task, changeset: changeset)
@@ -52,11 +68,16 @@ defmodule BugzillaWeb.TaskController do
   end
 
   def delete(conn, %{"id" => id}) do
-    task = Tasks.get_task!(id)
+    user = conn.assigns.current_user
+    task = Tasks.get_task!(id, user: user)
+    story_id = conn |> get_session(:current_story_id)
+    story = Stories.get_story_and_project!(story_id, user: user)
+    project = story.project
+
     {:ok, _task} = Tasks.delete_task(task)
 
     conn
     |> put_flash(:info, "Task deleted successfully.")
-    |> redirect(to: Routes.task_path(conn, :index))
+    |> redirect(to: Routes.project_story_path(conn, :edit, project, story))
   end
 end
