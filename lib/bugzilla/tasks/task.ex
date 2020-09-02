041ff5664a15a8f9109d4ab9f1778a82f1ab8982
Andrzej Trzaska <atrzaska2@gmail.com>
2020-09-02 20:31:38 +0200

    Add user id to tasks

--- lib/bugzilla/tasks/task.ex
+++ lib/bugzilla/tasks/task.ex
@@ -2,8 +2,15 @@ defmodule Bugzilla.Tasks.Task do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Bugzilla.Stories.Story
+  alias Bugzilla.Stories.User
+
   schema "tasks" do
     field :description, :string
+    field :complete, :boolean
+
+    belongs_to :story, Story
+    belongs_to :user, User
 
     timestamps()
   end
@@ -12,6 +19,6 @@ defmodule Bugzilla.Tasks.Task do
   def changeset(task, attrs) do
     task
     |> cast(attrs, [:description, :complete])
-    |> validate_required([:description])
+    |> validate_required([:description, :story_id, :user_id])
   end
 end
