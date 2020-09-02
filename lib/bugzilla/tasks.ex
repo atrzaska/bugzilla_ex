a6ad784c28ef53fe3b3b1988476d6666be53e88d
Andrzej Trzaska <atrzaska2@gmail.com>
2020-09-02 20:50:17 +0200

    New helper functions in tasks context

--- lib/bugzilla/tasks.ex
+++ lib/bugzilla/tasks.ex
@@ -8,10 +8,18 @@ defmodule Bugzilla.Tasks do
     Repo.all(Task)
   end
 
+  def list_tasks(story: story) do
+    from(t in Task, where: t.story_id == ^story.id) |> Repo.all()
+  end
+
+  def get_task!(id, user: user) do
+    Task |> Repo.get_by!(user_id: user.id, id: id)
+  end
+
   def get_task!(id), do: Repo.get!(Task, id)
 
-  def create_task(attrs \\ %{}) do
-    %Task{}
+  def create_task(attrs \\ %{}, user: user, story: story) do
+    %Task{user_id: user.id, story_id: story.id}
     |> Task.changeset(attrs)
     |> Repo.insert()
   end
