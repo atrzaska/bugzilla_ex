7dbcf660182023d2510aa17d4d899eb11e5c4ec0
Andrzej Trzaska <atrzaska2@gmail.com>
2020-09-03 20:00:52 +0200

    Add helper functions to send emails

--- lib/bugzilla/accounts/user_notifier.ex
+++ lib/bugzilla/accounts/user_notifier.ex
@@ -1,19 +1,19 @@
 defmodule Bugzilla.Accounts.UserNotifier do
-  alias Bugzilla.{Email, Mailer}
+  alias Bugzilla.Mailer
 
   def deliver_welcome_email(user) do
-    {:ok, Email.welcome(user) |> Mailer.deliver_later()}
+    Mailer.deliver_welcome_email(user)
   end
 
   def deliver_confirmation_instructions(user, url) do
-    {:ok, Email.confirmation(user, url) |> Mailer.deliver_later()}
+    Mailer.deliver_confirmation_instructions(user, url)
   end
 
   def deliver_reset_password_instructions(user, url) do
-    {:ok, Email.reset_password_instructions(user, url) |> Mailer.deliver_later()}
+    Mailer.deliver_reset_password_instructions(user, url)
   end
 
   def deliver_update_email_instructions(user, url) do
-    {:ok, Email.update_email_instructions(user, url) |> Mailer.deliver_later()}
+    Mailer.deliver_update_email_instructions(user, url)
   end
 end
--- lib/bugzilla/mailer.ex
+++ lib/bugzilla/mailer.ex
@@ -1,3 +1,25 @@
 defmodule Bugzilla.Mailer do
   use Bamboo.Mailer, otp_app: :bugzilla
+
+  alias Bugzilla.Email
+
+  def deliver_welcome_email(user) do
+    {:ok, Email.welcome(user) |> deliver_later()}
+  end
+
+  def deliver_confirmation_instructions(user, url) do
+    {:ok, Email.confirmation(user, url) |> deliver_later()}
+  end
+
+  def deliver_reset_password_instructions(user, url) do
+    {:ok, Email.reset_password_instructions(user, url) |> deliver_later()}
+  end
+
+  def deliver_update_email_instructions(user, url) do
+    {:ok, Email.update_email_instructions(user, url) |> deliver_later()}
+  end
+
+  def deliver_invitation_email(invite) do
+    {:ok, Email.invite(invite) |> deliver_later()}
+  end
 end
